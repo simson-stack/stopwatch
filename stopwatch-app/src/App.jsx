d655b8ab55f08bdd05b009dad0e5622e5af61614
Simas G <simas@simson.lt>
2024-11-10 21:11:13 +0200

    first commit

---
+++ stopwatch-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Stopwatch from "./Stopwatch.jsx"
+
+function App() {
+
+  return (
+      <>
+        <Stopwatch/>
+        <Stopwatch/>
+        <Stopwatch/>
+        <Stopwatch/>
+      </>
+  )
+}
+
+export default App
